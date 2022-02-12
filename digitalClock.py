f5d71dade92bd380ede2adbf141fd17b5fd07ba6
somaathetechster <somachukwu68@gmail.com>
2022-02-12 18:33:08 +0100

    finished Project

--- digitalClock.py
+++ digitalClock.py
@@ -4,9 +4,9 @@ from datetime import datetime
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="black")
 
-label = Label(window, font=("Arial Black",78,"bold"), bg="steelblue", fg="white")
+label = Label(window, font=("Arial Black",78,"bold"), bg="black", fg="steelblue")
 label.pack(pady=100)
 
 def clock():
